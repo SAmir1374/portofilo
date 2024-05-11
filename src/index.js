2ecd076135fbd100a5ffcbfdf132cb2d1103ccc6
Amirmuhammad <amirmohammadaghayanir@gmail.com>
2024-05-11 13:16:58 +0330

    Demo repear

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <I18nextProvider i18n={i18next}>
-      <BrowserRouter>
+      <BrowserRouter basename="/portofilo" >
         <App />
       </BrowserRouter>
     </I18nextProvider>
